12faa443d84e2ebb809e3304ec9d0d1459881266
shinysw <shinyswampert123@gmail.com>
2024-02-17 12:58:44 -0800

    fix

--- main.py
+++ main.py
@@ -37,32 +37,38 @@ def main():
     arduino_communicator = ArduinoSerialCommunicator()
 
     #example of using micro and speakers
-    print("Start recording audio")
-    sample_name = "aaaa.wav"
-    cmd = f'arecord -vv --format=cd --device={os.environ["AUDIO_INPUT_DEVICE"]} -r 48000 --duration=10 -c 1 {sample_name}'
-    print(cmd)
-    os.system(cmd)
-    print("Playing sound")
-    os.system(f"ffplay -nodisp -autoexit -loglevel quiet {sample_name}")
+    # print("Start recording audio")
+    # sample_name = "aaaa.wav"
+    # cmd = f'arecord -vv --format=cd --device={os.environ["AUDIO_INPUT_DEVICE"]} -r 48000 --duration=10 -c 1 {sample_name}'
+    # print(cmd)
+    # os.system(cmd)
+    # print("Playing sound")
+    # os.system(f"ffplay -nodisp -autoexit -loglevel quiet {sample_name}")
 
-    # Capture image
-    camera_capture = cv2.VideoCapture(0)
-    rv, image = camera_capture.read()
-    print(f"Image Dimensions: {image.shape}")
-    camera_capture.release()
+    # # Capture image
+    # camera_capture = cv2.VideoCapture(0)
+    # rv, image = camera_capture.read()
+    # print(f"Image Dimensions: {image.shape}")
+    # camera_capture.release()
 
     # Use wrapper in context manager to lease control, turn on E-Stop, power on the robot and stand up at start
     # and to return lease + sit down at the end
-    with SpotController(username=SPOT_USERNAME, password=SPOT_PASSWORD, robot_ip=ROBOT_IP) as spot:
-        time.sleep(2)
+    # with SpotController(username=SPOT_USERNAME, password=SPOT_PASSWORD, robot_ip=ROBOT_IP) as spot:
+    #     time.sleep(2)
 
-        spot.move_head_in_points(yaws=[0.2, 0], pitches=[0.3, 0], rolls=[0.4, 0], sleep_after_point_reached=1)
-        time.sleep(3)
+    #     spot.move_head_in_points(yaws=[0.2, 0], pitches=[0.3, 0], rolls=[0.4, 0], sleep_after_point_reached=1)
+    #     time.sleep(3)
 
 
+    #     arduino_communicator.send_message("Test Message")
+    #     response = arduino_communicator.receive_message()
+    #     print("Received from Arduino:", response)
+
+    for _ in range(20):
         arduino_communicator.send_message("Test Message")
         response = arduino_communicator.receive_message()
         print("Received from Arduino:", response)
+        time.sleep(1)  # Wait for a second before the next iteration
 
 
     arduino_communicator.close()
